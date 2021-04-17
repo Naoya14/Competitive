7fddc8894062872b131613a58bb2db40810ebb3f
Naoya Tabata <naoya1059@gmail.com>
2021-04-17 21:40:21 +0900

    add

---
+++ AtCoder/ARC/104a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  int a, b;
+  cin >> a >> b;
+
+  int x, y;
+  x = (a + b) / 2;
+
+  y = (a - b) / 2;
+
+  cout << x << " " << y;
+  return 0;
+}
